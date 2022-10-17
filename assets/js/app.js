e2641e6ef2f14f9e3be9a863e243c6d9255fd52d
JayBee-K <79182784+JayBee-K@users.noreply.github.com>
2022-10-17 17:11:01 +0700

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,25 @@
 	'use strict';
 	let windowWidth = $(window).width();
 
+	let initSlideBanner = function () {
+		if ($('#slideBanner').length > 0) {
+			new Swiper('#slideBanner', {
+				loop: false,
+				simulateTouch: false,
+				speed: 450,
+				navigation: {
+					nextEl: '#slideBanner .slide-button-next',
+					prevEl: '#slideBanner .slide-button-prev',
+				},
+				autoplay: {
+					delay: 10000,
+					disableOnInteraction: false,
+				},
+			});
+		}
+	}
+
 	$(function () {
+		initSlideBanner();
 	});
 })(jQuery);
