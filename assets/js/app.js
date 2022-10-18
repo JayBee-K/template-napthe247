a1df4fdb8c59ec1c1ce1c91eb86b708f9069b744
JayBee-K <79182784+JayBee-K@users.noreply.github.com>
2022-10-18 14:14:19 +0700

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,6 @@
 		if ($('#slideBanner').length > 0) {
 			new Swiper('#slideBanner', {
 				loop: false,
-				simulateTouch: false,
 				speed: 450,
 				navigation: {
 					nextEl: '#slideBanner .slide-button-next',
@@ -20,7 +19,55 @@
 		}
 	}
 
+	let initSlidePartner = function () {
+		if ($('#slidePartner').length > 0) {
+			new Swiper('#slidePartner .swiper', {
+				loop: false,
+				speed: 450,
+				spaceBetween: 30,
+				navigation: {
+					nextEl: '#slidePartner .slide-button-next',
+					prevEl: '#slidePartner .slide-button-prev',
+				},
+				autoplay: {
+					delay: 6000,
+					disableOnInteraction: false,
+				},
+				breakpoints: {
+					1200: {
+						slidesPerView: 6,
+					},
+				},
+			});
+		}
+	}
+
+	let initScrollTop = function () {
+		let windowHeight = $('body').height(),
+			btnReturn = $('#scroll-top');
+		$(window).scroll(function () {
+			let ratioHeight = (parseInt($(document).scrollTop()) / parseInt(windowHeight) * 100);
+
+			if (ratioHeight > 12.5) {
+				btnReturn.addClass('is-show');
+			} else {
+				btnReturn.removeClass('is-show');
+			}
+		});
+
+		btnReturn.click(function () {
+			$('html').css('scroll-behavior', 'auto');
+			$('html, body').animate({
+				scrollTop: 0
+			}, function () {
+				$('html').css('scroll-behavior', 'smooth');
+			});
+		});
+	}
+
 	$(function () {
 		initSlideBanner();
+		initSlidePartner();
+		initScrollTop();
 	});
 })(jQuery);
